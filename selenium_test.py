d308b4f55d96002ca2822d3abc96e2d5bf36ac00
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-04-26 10:03:04 -0300

    Adicionando Comentarios para entregar | Teste com Google

--- selenium_test.py
+++ selenium_test.py
@@ -3,29 +3,25 @@ from selenium.webdriver.common.by import By
 from pathlib import Path
 import os
 
+# Inicializa o recurso selenium
 driver = webdriver.Firefox()
 
+#Inicializa os parametros de busca
 web_site = "https://e2etec.com.br"
 search_pattern =  "//div[@id='panel-w5f11b62f8fe0c-0-0-0']/div[1]/div[1]/p[1]"
 
 driver.get(web_site)
-
 telefoneEmail = driver.find_element(
     By.XPATH, search_pattern)
+driver.implicitly_wait(2)
 
-driver.implicitly_wait(5)
-# debug
-# print(telefoneEmail.text)
-
+#Prepara os dados para criar o arquivo txt
 pasta = Path("c:/temp/")
-
 if not os.path.exists(pasta):
     os.makedirs(pasta)
-
 arquivo = pasta / "e2etec_py.txt"
-
 with open(arquivo, 'w+') as file:
     file.write(telefoneEmail.text)
-
+#Encerra o recusro selenium
 driver.close()
 
